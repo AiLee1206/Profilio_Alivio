3fa88dce7ae6cc37257af941f5d79b8338b4881b
AiLee <sharon1206_6@hotmail.com>
2021-07-28 21:33:43 +0800

    Initialize Profilio

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+// const prev = document.querySelector(".previous");
+// const nex = document.querySelector(".next");
+// const portfolio = document.querySelector("#portfolio");
+
+// prev.addEventListener("click", sliderfunction);
+
+// function sliderfunction() {
+//   const portfolioChildren =  event.target.parentElement.children[0];
+//   const portfolioImg =  portfolioChildren.children[0].src;
+//   console.log(portfolioImg);
+// }
