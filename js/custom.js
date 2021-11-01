54ac01fe2d0ad83a1f6593a1e9a7d68aa15f2469
AiLee <sharon1206_6@hotmail.com>
2021-11-01 18:30:13 +0800

    Modify go to top and css style.

--- js/custom.js
+++ js/custom.js
@@ -37,3 +37,24 @@ $('#careers').click(function(){
 $('#personalJobs').click(function(){
   $(this).parent().find('ul').toggle('slow');         
 });
+
+// go to top
+//Get the button
+var mybutton = document.getElementById("gotoTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
